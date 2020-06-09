970b95adf51d877be629fed7f3c9e33d3aa682a2
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-06-09 06:34:38 +0530

    feat: solved question 1

--- Algorithmic-Toolbox/Week6/assignment/1_maximum_amount_of_gold.cpp
+++ Algorithmic-Toolbox/Week6/assignment/1_maximum_amount_of_gold.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include <vector>
 
+typedef long long ll;
+
 using namespace std;
-int max_gold(int W, vector<int>& w)
+ll max_gold(int W, vector<int>& weights, vector<int> values)
 {
-    
-    return int {};
+    // n : no. of weights
+    int n = weights.size();
+    vector<vector<ll>> dp (n+1, vector<ll>(W+1, 0));
+    for(int i = 1; i < n+1; i++){
+        for(int w = 1; w < W+1; w++){
+            dp[i][w] = dp[i-1][w]; // if ith weight is not in optimal solution
+            if(weights[i-1] <= w){
+                ll value = dp[i-1][w-weights[i-1]] + values[i-1]; // if ith weight is in optimal solution
+                if(value > dp[i][w]){
+                    dp[i][w] = value;
+                }
+            }
+        }
+    }
+    return dp[n][W];
 }
 int main(void)
 {
-    int w, n;
-    cin >> w >> n;
-    vector<int> v;
+    int W, n;
+    cin >> W >> n;
+    vector<int> weights, values;
     for(int i = 0, a; i < n; i++){
         cin >> a;
-        v.push_back(a);
+        values.push_back(a);
+        weights.push_back(a);
     }
-    cout << max_gold(w, v);
+    cout << max_gold(W, weights, values);
     return 0;
 }
