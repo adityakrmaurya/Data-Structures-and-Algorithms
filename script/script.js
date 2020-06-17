1c543fbf4f8467cb7f4eb3beb82064abf59549af
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-06-17 15:24:55 +0530

    style: spelling mistake

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
+	const nav = document.querySelector('.nav-links');	
 	burger.addEventListener('click', () => {
 		nav.classList.toggle('nav-active');
 	});
