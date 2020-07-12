72ab1fded4257ecd882bea89572ef2e9412b985c
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-07-13 02:26:08 +0530

    feat: Solved question1

--- Data-Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
+++ Data-Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
@@ -21,25 +21,33 @@ struct Bracket {
     char type;
     int position;
 };
-
 int main() {
     std::string text;
     getline(std::cin, text);
 
     std::stack <Bracket> opening_brackets_stack;
-    for (int position = 0; position < text.length(); ++position) {
+    for (unsigned int long position = 0; position < text.length(); ++position) {
         char next = text[position];
 
         if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
+            opening_brackets_stack.push(Bracket(next, position));
         }
 
         if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
+            if(!opening_brackets_stack.empty() && opening_brackets_stack.top().Matchc(next)) {
+                opening_brackets_stack.pop();
+            }
+            else {
+                std::cout << (position + 1) << std::endl;
+                return 0;
+            }
         }
     }
-
-    // Printing answer, write your code here
-
+    if (opening_brackets_stack.empty()) {
+        std::cout << "Success" << std::endl;
+    }
+    else {
+        std::cout << (opening_brackets_stack.top().position + 1) << std::endl;
+    }
     return 0;
 }
