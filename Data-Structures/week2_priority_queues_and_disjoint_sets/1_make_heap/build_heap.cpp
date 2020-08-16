871d9e9a6f43d1c97ce1378725f91d06a369a174
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-08-16 08:08:13 +0530

    feat: Solved build_heap

--- Data-Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
+++ Data-Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
@@ -1,53 +1,63 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-using std::vector;
 using std::cin;
 using std::cout;
-using std::swap;
-using std::pair;
 using std::make_pair;
+using std::pair;
+using std::swap;
+using std::vector;
+
+template <typename T> void exch(T &a, T &b) {
+  T temp = a;
+  a = b;
+  b = temp;
+}
 
 class HeapBuilder {
- private:
-  vector<int> data_;
-  vector< pair<int, int> > swaps_;
+private:
+  int data[1000000];
+  int n;
+  vector<pair<int, int>> swaps_;
+
+  void fixdown(int pq[], int k, int size) {
+    while (2 * k <= size) {
+      int j = 2 * k;
+      if (j < size && pq[j] > pq[j + 1]) {
+        j++;
+      }
+      if (pq[j] >= pq[k]) {
+        break;
+      }
+      swaps_.push_back(make_pair(k - 1, j - 1));
+      exch(pq[j], pq[k]);
+      k = j;
+    }
+  }
 
   void WriteResponse() const {
     cout << swaps_.size() << "\n";
-    for (int i = 0; i < swaps_.size(); ++i) {
+    for (unsigned long long i = 0; i < swaps_.size(); ++i) {
       cout << swaps_[i].first << " " << swaps_[i].second << "\n";
     }
   }
 
   void ReadData() {
-    int n;
     cin >> n;
-    data_.resize(n);
-    for(int i = 0; i < n; ++i)
-      cin >> data_[i];
+    for (int i = 0; i < n; ++i)
+      cin >> data[i];
   }
 
   void GenerateSwaps() {
     swaps_.clear();
-    // The following naive implementation just sorts 
-    // the given sequence using selection sort algorithm
-    // and saves the resulting sequence of swaps.
-    // This turns the given array into a heap, 
-    // but in the worst case gives a quadratic number of swaps.
-    //
-    // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
-        }
-      }
+    int *pq = data - 1;
+    for (int i = n / 2; i >= 1; i--) {
+      fixdown(pq, i, n);
+    }
   }
 
- public:
+public:
   void Solve() {
     ReadData();
     GenerateSwaps();
