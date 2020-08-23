e807039aa1561a8b45d865ffd498bc954cbe2a74
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-08-24 00:38:29 +0530

    feat: Completed tree-orders

--- Data-Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.cpp
+++ Data-Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.cpp
@@ -1,20 +1,20 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #if defined(__unix__) || defined(__APPLE__)
 #include <sys/resource.h>
 #endif
 
-using std::vector;
-using std::ios_base;
 using std::cin;
 using std::cout;
+using std::ios_base;
+using std::vector;
 
 class TreeOrders {
   int n;
-  vector <int> key;
-  vector <int> left;
-  vector <int> right;
+  vector<int> key;
+  vector<int> left;
+  vector<int> right;
 
 public:
   void read() {
@@ -27,75 +27,65 @@ public:
     }
   }
 
-
-  vector <int> in_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-
-    return result;
+  void in_order(int ind) {
+    if (ind == -1) {
+      return;
+    }
+    in_order(left[ind]);
+    cout << key[ind] << " ";
+    in_order(right[ind]);
   }
-
-  vector <int> pre_order() {
-    vector<int> result;    
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
-    return result;
+  void pre_order(int ind) {
+    if (ind == -1) {
+      return;
+    }
+    cout << key[ind] << " ";
+    pre_order(left[ind]);
+    pre_order(right[ind]);
   }
-
-  vector <int> post_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
-    return result;
+  void post_order(int ind) {
+    if (ind == -1) {
+      return;
+    }
+    post_order(left[ind]);
+    post_order(right[ind]);
+    cout << key[ind] << " ";
   }
 };
-
-void print(vector <int> a) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (i > 0) {
-      cout << ' ';
-    }
-    cout << a[i];
-  }
-  cout << '\n';
-}
 
 int main_with_large_stack_space() {
   ios_base::sync_with_stdio(0);
   TreeOrders t;
   t.read();
-  print(t.in_order());
-  print(t.pre_order());
-  print(t.post_order());
+  t.in_order(0);
+
+  cout << std::endl;
+  t.pre_order(0);
+
+  cout << std::endl;
+  t.post_order(0);
+  cout << std::endl;
   return 0;
 }
 
-int main (int argc, char **argv)
-{
+int main(int argc, char **argv) {
 #if defined(__unix__) || defined(__APPLE__)
   // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
+  const rlim_t kStackSize = 16 * 1024 * 1024; // min stack size = 16 MB
   struct rlimit rl;
   int result;
 
   result = getrlimit(RLIMIT_STACK, &rl);
-  if (result == 0)
-  {
-      if (rl.rlim_cur < kStackSize)
-      {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
+  if (result == 0) {
+    if (rl.rlim_cur < kStackSize) {
+      rl.rlim_cur = kStackSize;
+      result = setrlimit(RLIMIT_STACK, &rl);
+      if (result != 0) {
+        std::cerr << "setrlimit returned result = " << result << std::endl;
       }
+    }
   }
 #endif
 
   return main_with_large_stack_space();
 }
-
